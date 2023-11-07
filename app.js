4948a30bb0b710753affdc795c4d38824f3f5565
Axel Aguilar <axel.aguilar@gmail.se>
2023-11-07 21:11:07 +0100

    add event handler for load event

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const skapaIframe = document.getElementById('skapa-iframe');
-const openWindow = document.getElementById('open-window');
-const closeWindow = document.getElementById('close-window');
-let embeddedWindow;
-
-skapaIframe.addEventListener('click', () => {
-  console.log('iframe');
-  const iframe = document.createElement('iframe');
-  iframe.src = 'https://rad-choux-97d2d8.netlify.app/';
-  iframe.style.width = '300px';
-  iframe.style.height = '200px';
-  document.body.appendChild(iframe);
-});
-
-openWindow.addEventListener('click', () => {
-  console.log('window.open');
-  embeddedWindow = window.open(
-    "https://rad-choux-97d2d8.netlify.app/",
-    "_blank",
-    "width=300,height=200,resizable=no,scrollbars=no,left=300,top=150"
-  );  
-});
-
-closeWindow.addEventListener('click', () => {
-  embeddedWindow.close();
+window.addEventListener("load", () => {
+    const skapaIframe = document.getElementById('skapa-iframe');
+    const openWindow = document.getElementById('open-window');
+    const closeWindow = document.getElementById('close-window');
+    let embeddedWindow;
+    
+    skapaIframe.addEventListener('click', () => {
+      console.log('iframe');
+      const iframe = document.createElement('iframe');
+      iframe.src = 'https://rad-choux-97d2d8.netlify.app/';
+      iframe.style.width = '300px';
+      iframe.style.height = '200px';
+      document.body.appendChild(iframe);
+    });
+    
+    openWindow.addEventListener('click', () => {
+      console.log('window.open');
+      embeddedWindow = window.open(
+        "https://rad-choux-97d2d8.netlify.app/",
+        "_blank",
+        "width=300,height=200,resizable=no,scrollbars=no,left=300,top=150"
+      );  
+    });
+    
+    closeWindow.addEventListener('click', () => {
+      embeddedWindow.close();
+    });
+    
 });
