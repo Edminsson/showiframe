48f62034f5187982e5eab8929fc6440c96d3f7b0
Axel Aguilar <axel.aguilar@gmail.se>
2023-11-07 21:43:18 +0100

    remove iframe

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 window.addEventListener("load", () => {
-    const skapaIframe = document.getElementById('skapa-iframe');
+    const skapaIframe = document.getElementById('create-iframe');
+    const raderaIframe = document.getElementById('remove-iframe');
     const openWindow = document.getElementById('open-window');
     const closeWindow = document.getElementById('close-window');
     let embeddedWindow;
     
     skapaIframe.addEventListener('click', () => {
-      console.log('iframe');
+      console.log('create iframe');
       const iframe = document.createElement('iframe');
+      iframe.id = 'iframe'
       iframe.src = 'https://rad-choux-97d2d8.netlify.app/';
       iframe.style.width = '500px';
       iframe.style.height = '500px';
       document.body.appendChild(iframe);
     });
-    
+
+    skapaIframe.addEventListener('click', () => {
+      console.log('remove iframe');
+      const iFrameEl = document.getElementById('iframe');
+      document.body.removeChild(iFrameEl);
+    });    
+
     openWindow.addEventListener('click', () => {
       console.log('window.open');
       embeddedWindow = window.open(
