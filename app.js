71c0d8da8ed5020a7ad0918f423af81eae15d048
Axel Aguilar <axel.aguilar@gmail.se>
2023-11-07 20:52:54 +0100

    add app

---
+++ app.js
@@ -0,0 +1,33 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Iframe och window.open</h1>`;
+
+const skapaIframe = document.getElementById('skapa-iframe');
+const openWindow = document.getElementById('open-window');
+const closeWindow = document.getElementById('close-window');
+let embeddedWindow;
+
+skapaIframe.addEventListener('click', () => {
+  console.log('iframe');
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://rad-choux-97d2d8.netlify.app/';
+  iframe.style.width = '300px';
+  iframe.style.height = '200px';
+  document.body.appendChild(iframe);
+});
+
+openWindow.addEventListener('click', () => {
+  console.log('window.open');
+  embeddedWindow = window.open(
+    "https://rad-choux-97d2d8.netlify.app/",
+    "_blank",
+    "width=300,height=200,resizable=no,scrollbars=no,left=300,top=150"
+  );  
+});
+
+closeWindow.addEventListener('click', () => {
+  embeddedWindow.close();
+});
